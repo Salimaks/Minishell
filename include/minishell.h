7808c381aec6e5ecb50c5b62935a30bf62cd8365
Alex Kling <mkling@student.42.fr>
2024-11-27 16:31:12 +0100

    modified command_table struct to get all required elements for execution

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skassimi <skassimi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mkling <mkling@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 12:02:49 by skassimi          #+#    #+#             */
-/*   Updated: 2024/11/27 12:04:16 by skassimi         ###   ########.fr       */
+/*   Updated: 2024/11/27 16:30:09 by mkling           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,32 @@
 
 typedef struct s_cmd
 {
-	char	**argv;
 	int		argc;
-	char	*outfile;
-	char	*infile;
+	char	**argv;		// array created with cmd_path, then all arguments
+	char	**env;		// needed by executing fork for execve()
+	char	*outfile;	// filepath from which input must be redirected
+	char	*infile;	// filepath to which output must be redirected
+	char	*cmd_path;	// binary filepath, absolute or through env's PATH
+	int		exit_code;	// value returned by the execution of the command
+	int		cmd_index;	// number of the command among the other commands
 	s_cmd	*next;
 }	t_cmd;
 
-typedef struct s_command_table
+typedef struct	s_cmd_table
 {
-    t_cmd *first;
-    int cmd_count;
-}    t_command_table;
+	t_cmd	*first_cmd;	// start of the linked list of commands
+	int		cmd_count;	// total of commands in commmand line
+	int		curr_cmd;	// command currently being executed
+	int		*pipefd[2];	// array of pipe fd [2] required by each pipe()
+	char	**path;		// extracted PATH variable of the env
+	char	**env;		// env received at start of program
+}	t_cmd_tab;
 
 typedef struct s_maman
 {
-    t_command_table *cmd;
-    char **line;
-    char **env;
-}    t_maman
+	char		**line;
+	t_cmd_tab	cmd_tab;	// struct organizing and keeping count of cmds
+	char		**env;		// env received at start of program
+}	t_maman;
 
 #endif
